1678f0d561d62fc9662ec6534d7dbe09b3cb06ba
“DattaBansude” <dattatraybansude91@gmail.com>
2025-08-30 12:37:28 +0530

    VEHMS-M01-T001: Initialized Spring Boot project structure

---
+++ src/main/java/com/parkify/vehitrack/ParkifyApplication.java
@@ -0,0 +1,14 @@
+package com.parkify.vehitrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkifyApplication.class, args);
+		System.out.println("Hello this project created by datta");
+	}
+
+}
---
+++ src/test/java/com/parkify/vehitrack/ParkifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkify.vehitrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
